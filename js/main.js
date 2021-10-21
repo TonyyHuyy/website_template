71c844edfbbff37443e931f6c7a03d29a93cb3d5
Huy <quach.huy2015@gmail.com>
2021-10-21 23:29:33 +0700

    upload source code full templatemo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+  "use strict";
+
+  $(".navigation").singlePageNav({
+    currentClass: "active",
+  });
+
+  $(".toggle-menu").click(function () {
+    $(".responsive-menu").stop(true, true).slideToggle();
+    return false;
+  });
+});
